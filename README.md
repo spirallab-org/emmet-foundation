33ccf221561e03e61a7143130e905c7656eab920
Daniel Kossmann Ferraz <daniel@spirallab.com.br>
2013-03-31 10:45:44 -0300

    Added centered class to all grid sized. Now snippets.json can be generated using generate.py.

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 Emmet ( http://emmet.io/ ) extension for Zurb Foundation 4 ( http://foundation.zurb.com/ )
 
 ## Features
-* Grid: large, small and large-small
+* Grid:
+	* Sizes: large, small and large-small
+	* Centered: large, small and large-small
 * Block Grid: large, small and large-small
 
-## How to
+## How to use
 
 ### Grid
 
@@ -38,6 +40,30 @@ d:r>d:s2l4
 ```html
 <div class="row">
 	<div class="small-2 large-4 columns"></div>
+</div> 
+```
+#### Small Columns Centered
+d:r>d:s2c
+&rarr;
+```html
+<div class="row">
+	<div class="small-2 small-centered columns"></div>
+</div>  
+```
+#### Large Columns
+d:r>d:l4c
+&rarr;
+```html
+<div class="row">
+	<div class="large-4 large-centered columns"></div>
+</div>  
+```
+#### Small and Large Columns Centered
+d:r>d:s2cl4c
+&rarr;
+```html
+<div class="row">
+	<div class="small-2 small-centered large-4 large-centered columns"></div>
 </div> 
 ```
 ### Block Grid
--- generate.py
+++ generate.py
@@ -5,12 +5,39 @@ columns = 12
 sizes = ['small', 'large']
 
 ######################################
+# file header
+print '''
+{
+    "variables": {
+        "lang": "en",
+        "locale": "en-US",
+        "charset": "UTF-8",
+        "indentation": "\\t",
+        "newline": "\\n"
+    },
+    
+    "html": {
+        "filters": "html",
+        "profile": "html",
+        "snippets": {
+        },
+        
+        "abbreviations": {
+            "d:r" : "<div class=\\"row\\">",
+'''
+######################################
 # block grid code
 
 # generate code for each size
 for size in sizes:
     for i in range(1,columns+1):
-        print '"d:' + size[0] + str(i) + '" : "<div class=\\"' + size + "-" + str(i) + ' columns\\">",'
+        for centered in [""]+[size]:
+            if centered:
+                centered = centered + "-centered "
+                prefix = "c"
+            else:
+                prefix = ""
+            print '\t\t\t"d:' + size[0] + str(i) + prefix + '" : "<div class=\\"' + size + "-" + str(i) + ' ' + centered +'columns\\">",'
     print # new line
     
 print # new line
@@ -18,7 +45,11 @@ print # new line
 # generate code for double size
 for i in range(1,columns+1):
     for j in range(1,columns+1):
-            print '"d:' + sizes[0][0] + str(i) + sizes[1][0] + str(j) + '" : "<div class=\\"' + sizes[0] + "-" + str(i) + " " + sizes[1] + "-" + str(j) + ' columns\\">",'
+        print '\t\t\t"d:' + sizes[0][0] + str(i) + sizes[1][0] + str(j) + '" : "<div class=\\"' + sizes[0] + "-" + str(i) + " " + sizes[1] + "-" + str(j) + ' columns\\">",'
+        print '\t\t\t"d:' + sizes[0][0] + str(i) + "c" + sizes[1][0] + str(j) + '" : "<div class=\\"' + sizes[0] + "-" + str(i) + " small-centered " + sizes[1] + "-" + str(j) + ' columns\\">",'
+        print '\t\t\t"d:' + sizes[0][0] + str(i) + sizes[1][0] + str(j) + "c" + '" : "<div class=\\"' + sizes[0] + "-" + str(i) + " " + sizes[1] + "-" + str(j) + ' large-centered columns\\">",'
+        print '\t\t\t"d:' + sizes[0][0] + str(i) + "c" + sizes[1][0] + str(j) + "c" + '" : "<div class=\\"' + sizes[0] + "-" + str(i) + " small-centered " + sizes[1] + "-" + str(j) + ' large-centered columns\\">",'
+            
 
 print # new line
 
@@ -28,7 +59,7 @@ print # new line
 # generate code for each size
 for size in sizes:
     for i in range(1,columns+1):
-        print '"u:' + size[0] + str(i) + '" : "<ul class=\\"' + size + "-block-grid-" + str(i) + '\\">",'
+        print '\t\t\t"u:' + size[0] + str(i) + '" : "<ul class=\\"' + size + "-block-grid-" + str(i) + '\\">",'
     print # new line
     
 print # new line
@@ -36,4 +67,11 @@ print # new line
 # generate code for double size
 for i in range(1,columns+1):
     for j in range(1,columns+1):
-            print '"u:' + sizes[0][0] + str(i) + sizes[1][0] + str(j) + '" : "<ul class=\\"' + sizes[0] + "-block-grid-" + str(i) + " " + sizes[1] + "-block-grid-" + str(j) + '\\">",'
+            print '\t\t\t"u:' + sizes[0][0] + str(i) + sizes[1][0] + str(j) + '" : "<ul class=\\"' + sizes[0] + "-block-grid-" + str(i) + " " + sizes[1] + "-block-grid-" + str(j) + '\\">",'
+######################################
+# file footer
+print '''
+        }, 
+    },
+}
+'''
